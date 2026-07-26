127995871ece300e8126454735ef6b6aeb2abf3e
Rika Amamiya <179112848+Mamiki765@users.noreply.github.com>
2026-07-26 16:58:52 +0900

    fix: isolate container tests from production database

--- product/tests/TestCase.php
+++ product/tests/TestCase.php
@@ -5,11 +5,19 @@ namespace Tests;
 use Illuminate\Database\SQLiteConnection;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Illuminate\Support\Facades\DB;
+use RuntimeException;
 
 abstract class TestCase extends BaseTestCase
 {
     protected function setUp(): void
     {
+        $connection = (string) getenv('DB_CONNECTION');
+        $database = (string) getenv('DB_DATABASE');
+
+        if ($connection === 'pgsql' && ! str_ends_with($database, '_test')) {
+            throw new RuntimeException("Refusing to run tests against PostgreSQL database [{$database}].");
+        }
+
         parent::setUp();
 
         $connection = DB::connection();
