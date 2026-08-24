a220b122438decf870ecf9d440e1da2dd8ed7863
Mamiki765 <179112848+Mamiki765@users.noreply.github.com>
2026-08-24 23:14:44 +0900

    chore: finalize application version 2.5.0

--- product/config/hakoniwa.php
+++ product/config/hakoniwa.php
@@ -3,7 +3,7 @@
 $currentRuleset = require __DIR__.'/hakoniwa/rulesets/hakoniwa-2s-plus-v15.php';
 
 return [
-    'application_version' => '2.5.0-beta',
+    'application_version' => '2.5.0',
     'ruleset' => $currentRuleset,
     'published_rulesets' => [$currentRuleset['key'] => $currentRuleset],
     'current_catalogs' => [
