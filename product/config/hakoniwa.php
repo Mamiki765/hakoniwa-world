368eadf919b599f104e1cbc35d3d8604733f5284
Rika Amamiya <179112848+Mamiki765@users.noreply.github.com>
2026-09-07 13:18:31 +0900

    chore: prepare 3.7.3 candidate version

--- product/config/hakoniwa.php
+++ product/config/hakoniwa.php
@@ -3,7 +3,7 @@
 $currentRuleset = require __DIR__.'/hakoniwa/rulesets/hakoniwa-2s-plus-v21.php';
 
 return [
-    'application_version' => '3.7.2',
+    'application_version' => '3.7.3',
     'ruleset' => $currentRuleset,
     'published_rulesets' => [$currentRuleset['key'] => $currentRuleset],
     'current_catalogs' => [
