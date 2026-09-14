313c60ab8a470bbd2f7a0160afc9cc2a0a200bb5
Rika Amamiya <179112848+Mamiki765@users.noreply.github.com>
2026-09-14 21:00:08 +0900

    test: align fresh install version for 4.1.2

--- product/tests/Feature/FreshInstallRebaselineTest.php
+++ product/tests/Feature/FreshInstallRebaselineTest.php
@@ -42,7 +42,7 @@ final class FreshInstallRebaselineTest extends TestCase
         app(RulesetPublisher::class)->publish($current);
         $ruleset = RulesetVersion::query()->where('key', Ver393RulesetUpgrade::TARGET_KEY)->sole();
 
-        $this->assertSame('4.1.0', config('hakoniwa.application_version'));
+        $this->assertSame('4.1.2', config('hakoniwa.application_version'));
         $this->assertSame([Ver393RulesetUpgrade::TARGET_KEY], array_keys(config('hakoniwa.published_rulesets')));
         $this->assertSame(Ver393RulesetUpgrade::TARGET_KEY, $ruleset->key);
         $this->assertSame(Ver393RulesetUpgrade::TARGET_VERSION, $ruleset->version);
